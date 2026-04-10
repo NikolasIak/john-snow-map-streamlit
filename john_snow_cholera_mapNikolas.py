d857f6b7acb518dbb868e64dbb7bbbc8805961f3
NikolasIak <55096448+NikolasIak@users.noreply.github.com>
2026-04-10 17:18:48 +0300

    Update john_snow_cholera_mapNikolas.py

--- john_snow_cholera_mapNikolas.py
+++ john_snow_cholera_mapNikolas.py
@@ -1,78 +1,122 @@
-#developed by Fouad Zablith
-
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
 from PIL import Image
 
-#load the data from excel in a dateframe
-#a similar (with cleaner ) dataset is accessible online at https://docs.google.com/file/d/0B1k6zmQ4NXQlZ0dBcjFrNWhUSTA/edit
-data = pd.read_csv(r'CholeraPumps_Deaths.csv')
-df = pd.DataFrame(data, columns=['count','geometry'])
+st.set_page_config(layout="wide")
 
-#clean and replace the coordinates data to fit the PyDeck map 
-df = df.replace({'<Point><coordinates>': ''},regex=True )
-df = df.replace({'</coordinates></Point>': ''},regex=True )
+# -----------------------------
+# LOAD DATA (Improvement 1: caching)
+# -----------------------------
+@st.cache_data
+def load_data():
+    data = pd.read_csv('CholeraPumps_Deaths.csv')
+    df = pd.DataFrame(data, columns=['count','geometry'])
 
-#create new longitude and latitude columns in dataframe
-split = df['geometry'].str.split(',',n=1, expand=True)
-df['lon'] = split[0].astype(float)
-df['lat'] = split[1].astype(float)
+    df = df.replace({'<Point><coordinates>': ''}, regex=True)
+    df = df.replace({'</coordinates></Point>': ''}, regex=True)
 
-df.drop(columns=['geometry'], inplace = True)
+    split = df['geometry'].str.split(',', n=1, expand=True)
+    df['lon'] = split[0].astype(float)
+    df['lat'] = split[1].astype(float)
 
-#st.write(df)
+    df.drop(columns=['geometry'], inplace=True)
 
-st.header('John Snow\'s 1854 Cholera Deaths Map in London')
-st.subheader('This is a recreation of Snow\'s famous map that helped identifying the source of cholera oubreak in London')
+    return df
 
-#create a slider to filter the number of deaths
-death_to_filter = st.slider('Number of Deaths', 0, 15, 2)  # min: 0 death, max: 15 deaths, default: 7 deaths
-filtered_df = df[df['count'] >= death_to_filter]
-st.subheader(f'Map of more than {death_to_filter} deaths')
+df = load_data()
 
-#get the pumps location from the last 8 entries in the data (in the original data source, pumps were noted as having -999 death as a differentiator)
+# Separate pumps
 pumps_df = df[df['count'] == -999]
+deaths_df = df[df['count'] != -999]
 
-#checkbox to enable seeing the location of pumps
-if(st.checkbox('Show pumps',value=True)):
-    pump_radius = 5
-else:
-    pump_radius = 0
+# -----------------------------
+# SIDEBAR (Improvement 2: better UI)
+# -----------------------------
+st.sidebar.title("Controls")
+
+death_to_filter = st.sidebar.slider('Minimum number of deaths', 0, 15, 2)
+
+show_pumps = st.sidebar.checkbox('Show pumps', value=True)
+
+color = st.sidebar.color_picker("Choose death color", "#FF0000")
+
+# Convert hex to RGB
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+rgb_color = hex_to_rgb(color)
+
+# -----------------------------
+# FILTER DATA
+# -----------------------------
+filtered_df = deaths_df[deaths_df['count'] >= death_to_filter]
+
+# -----------------------------
+# MAIN PAGE
+# -----------------------------
+st.title("John Snow’s 1854 Cholera Map")
+st.markdown("Interactive visualization of cholera deaths and water pumps.")
+
+# -----------------------------
+# METRICS (Improvement 3)
+# -----------------------------
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric("Deaths shown", len(filtered_df))
+
+with col2:
+    st.metric("Pumps shown", len(pumps_df) if show_pumps else 0)
+
+# -----------------------------
+# MAP
+# -----------------------------
+layers = [
+    pdk.Layer(
+        'ScatterplotLayer',
+        data=filtered_df,
+        get_position='[lon, lat]',
+        get_color=f'[{rgb_color[0]}, {rgb_color[1]}, {rgb_color[2]}, 160]',
+        get_radius='count * 2',
+    )
+]
+
+if show_pumps:
+    layers.append(
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=pumps_df,
+            get_position='[lon, lat]',
+            get_color='[0, 0, 255, 160]',
+            get_radius=50,
+        )
+    )
 
 st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
+    map_style=None,
     initial_view_state=pdk.ViewState(
         latitude=51.5134,
         longitude=-0.1365,
         zoom=15.5,
         pitch=0,
     ),
-    layers=[
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=filtered_df,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius='[count]',
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=pumps_df,
-            get_position='[lon, lat]',
-            get_color='[0, 0, 255, 160]',
-            get_radius=pump_radius,
-        ),
-     ],
+    layers=layers,
 ))
 
-st.markdown('The red dots show the locations of deaths with a size reflecting the numbers of deaths, and the blue dots show the locations of water pumps.')
+# -----------------------------
+# DESCRIPTION
+# -----------------------------
+st.markdown("""
+- Colored dots represent cholera deaths  
+- Size reflects number of deaths  
+- Blue dots represent water pumps  
+""")
 
-image = Image.open('Snow-cholera-map-1.jpg')
-
+# -----------------------------
+# ORIGINAL IMAGE
+# -----------------------------
 st.subheader('Original map of John Snow')
-st.image(image,caption='Original map by John Snow showing the clusters of cholera cases in the London epidemic of 1854, drawn and lithographed by Charles Cheffins',use_column_width=True)
-
-st.markdown('The source of the above map and more details on John Snow\'s work can be found here: [https://en.wikipedia.org/wiki/John_Snow](https://en.wikipedia.org/wiki/John_Snow)')
-
-st.markdown("Developed by [Fouad Zablith](http://fouad.zablith.org). If you have any question about this simple app, you can reach me through: [@fzablith](https://twitter.com/fzablith)")
+image = Image.open('Snow-cholera-map-1.jpg')
+st.image(image, use_column_width=True)
